e6bde4bfb3667e389a8d1399a3ce7245c0c40576
Akira Kamikura <akira.kamikura@gmail.com>
2024-09-05 13:28:12 +0900

    change to C from C++

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-cmake -B build && cd build
-cmake .. -DBUILD_TESTS=ON
-cmake --build .
-ctest --verbose
--- tests/main.cc
+++
@@ -1,4 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(test, case1) { EXPECT_EQ(1, 1); }
-TEST(test, case2) { EXPECT_EQ(2, 2); }
