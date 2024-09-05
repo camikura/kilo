fb520a599de51d9c272c0e1afb8b1bd179b15b54
Akira Kamikura <akira.kamikura@gmail.com>
2024-09-05 15:51:55 +0900

    chapter2

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,68 @@
-#include <iostream>
+/*** includes ***/
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <iostream>
 #include <termios.h>
 
 using namespace std;
 
-void EnableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
+/*** data ***/
 
-  raw.c_lflag &= ~(ECHO);
+struct termios orig_termios;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+/*** terminal ***/
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
 }
+
+void DisableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
+}
+
+void EnableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    die("tcgetattr");
+  }
+  atexit(DisableRawMode);
+
+  struct termios raw = orig_termios;
+
+  // IXON: Ctrl-S and Ctrl-Q
+  // ICANON: canonical mode
+  // ISIG: Ctrl-C and Ctrl-Z signals
+  // IEXTEN: Ctrl-V
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    die("tcsetattr");
+  }
+}
+
+/*** init ***/
 
 int main() {
   EnableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+      die("read");
+    }
+
+    if (c == 'q') break;
+  }
 
   return 0;
 }
