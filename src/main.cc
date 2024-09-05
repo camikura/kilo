c1fb2944bd93292df0ba89f867c71188a76a7c92
Akira Kamikura <akira.kamikura@gmail.com>
2024-09-05 14:54:28 +0900

    canonical mode

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <unistd.h>
+#include <termios.h>
 
 using namespace std;
 
-int main() { cout << "Hello world!" << endl; }
+void EnableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+  EnableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+  return 0;
+}
