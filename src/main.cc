a21acad11d0f83185cd898db47039afc1971f8f3
Akira Kamikura <akira.kamikura@gmail.com>
2024-09-10 17:21:38 +0900

    end Chapter 3

--- src/main.cc
+++ src/main.cc
@@ -22,6 +22,9 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -96,10 +99,20 @@ int EditorReadKey() {
           return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
+            case '1':
+              return HOME_KEY;
+            case '3':
+              return DEL_KEY;
+            case '4':
+              return END_KEY;
             case '5':
               return PAGE_UP;
             case '6':
               return PAGE_DOWN;
+            case '7':
+              return HOME_KEY;
+            case '8':
+              return END_KEY;
           }
         }
       } else {
@@ -112,7 +125,18 @@ int EditorReadKey() {
             return ARROW_RIGHT;
           case 'D':
             return ARROW_LEFT;
+          case 'H':
+            return HOME_KEY;
+          case 'F':
+            return END_KEY;
         }
+      }
+    } else if (seq[0] == '0') {
+      switch (seq[1]) {
+        case 'H':
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
       }
     }
     return '\x1b';
@@ -215,6 +239,14 @@ void EditorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
+      break;
+
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+
+    case END_KEY:
+      E.cx = E.screencols - 1;
       break;
 
     case PAGE_UP:
